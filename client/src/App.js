f6cda94f9af2ab2fafbd94dbd880e585cb9ae23c
volkanGoksu <volkan.goksu@outlook.com>
2021-05-31 23:28:17 +0300

    Searchbar styles

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,41 @@ function App() {
  })
   return (
      <div>
-       <h1>Hello world!</h1>
+       <Header>
+         <input placeholder = "Search items"></input>
+         <button><i className="fas fa-search"></i></button>
+       </Header>
      </div>
   );
 }
 
 export default App;
+
+const Header = styled.header`
+ display:flex;
+ min-height:5vh;
+ align-items:center;
+ justify-content:center;
+ border-bottom : 1px solid rgba(0,0,0,0.2);
+ font-size:1rem;
+
+ input {
+   padding : 0.5rem;
+   margin-right:5px;
+   width:300px;
+   outline:none;
+ }  
+
+ button {
+    padding:0.5rem 2 rem;
+    margin-right:5px;
+    transition: .2s ease;
+    cursor:pointer;
+ }
+
+ button:hover {
+   background : black;
+   color: white;
+   border: none;
+ }
+`
