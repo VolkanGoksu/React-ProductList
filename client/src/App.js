12476148dd5d83423448c9454e782f79b734cbc3
volkanGoksu <volkan.goksu@outlook.com>
2021-05-31 23:11:52 +0300

    fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
-
+import React ,{useState,useEffect}from 'react';
+import styled from 'styled-components';
 
 function App() {
+ const [items,setitems] = useState([]);
+ useEffect(async()=>{
+   const data = await fetch('https://fakestoreapi.com/products');
+   const result = await data.json();
+   console.log(result);
+ })
   return (
-
      <div>
        <h1>Hello world!</h1>
      </div>
